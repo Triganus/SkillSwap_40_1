d09c66377ee7cbd327e17fe77614822440c8b284
kravchenko_d <kravchenko_d@lwo.by>
2025-10-23 11:30:26 +0300

    feat: Добавлена базовая маршрутизация. additional: Добавлена базовая разметка и с некоторой логикой перехода по страница авторизации и регистрации.

---
+++ src/pages/profile/ui/ProfilePage.tsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import { useAuth } from '@app/Provider';
+
+export default function ProfilePage() {
+  const { auth, logout } = useAuth();
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      {auth.user ? (
+        <>
+          <p>
+            {auth.user.name}
+          </p>
+          <button onClick={logout}>Выйти</button>
+        </>
+      ) : (
+        <p>
+          Неизвестный пользователь. <Link to="/login">Войти</Link>
+        </p>
+      )}
+    </div>
+  );
+}
