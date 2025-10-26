623243e94e450fa2a919e8c970ca6f36154dc245
Panic <smusenko.77@gmail.com>
2025-10-26 15:44:32 +0100

    fix: use type-only import for TPreloaderUIProps

--- src/shared/ui/Preloader/PreloaderUI.tsx
+++ src/shared/ui/Preloader/PreloaderUI.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './PreloaderUI.module.scss';
-import { TPreloaderUIProps } from './TPreloaderUIProps';
+import type { TPreloaderUIProps } from './TPreloaderUIProps';
 
 export const PreloaderUI: React.FC<TPreloaderUIProps> = ({
   size = 'medium',
