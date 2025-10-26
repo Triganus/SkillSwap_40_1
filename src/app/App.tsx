e990a1a003f005013b3cae0eeda916428d22c62f
Vladimir Devyatov <devyatov2704@icloud.com>
2025-10-26 18:26:10 +0300

    add: layout add: footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,36 +1,29 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './App.css';
-import AppRouter from './router/AppRouter';
-import { useAuth } from './Provider';
+// import { useAuth } from './Provider';
+import { AppLayout } from './AppLayout';
 
-function Header() {
-  const { auth, logout } = useAuth();
-  return (
-    <header style={{ display: 'flex', gap: 20 }}>
-      <Link to="/">Home</Link>
-      <Link to="/favorites">Favorites</Link>
-      <Link to="/profile">Profile</Link>
-      <div style={{ marginLeft: 'auto' }}>
-        {auth.isAuthenticated ? (
-          <>
-            <span style={{ marginRight: 8 }}>{auth.user?.name}</span>
-            <button onClick={logout}>Logout</button>
-          </>
-        ) : (
-          <Link to="/login">Login</Link>
-        )}
-      </div>
-    </header>
-  );
-}
+// function Header() {
+//   const { auth, logout } = useAuth();
+//   return (
+//     <header style={{ display: 'flex', gap: 20 }}>
+//       <Link to="/">Home</Link>
+//       <Link to="/favorites">Favorites</Link>
+//       <Link to="/profile">Profile</Link>
+//       <div style={{ marginLeft: 'auto' }}>
+//         {auth.isAuthenticated ? (
+//           <>
+//             <span style={{ marginRight: 8 }}>{auth.user?.name}</span>
+//             <button onClick={logout}>Logout</button>
+//           </>
+//         ) : (
+//           <Link to="/login">Login</Link>
+//         )}
+//       </div>
+//     </header>
+//   );
+// }
 
 export default function App() {
-  return (
-    <div>
-      <Header />
-      <main>
-        <AppRouter />
-      </main>
-    </div>
-  );
+  return <AppLayout>здесь должен быть main</AppLayout>;
 }
