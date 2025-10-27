a5f7ec471b60012776ff9258c0c6fec88fd861d8
Alexander Ryabtsev <bro-88@mail.ru>
2025-10-27 18:24:33 +0300

    Icon component adjustement

--- src/shared/ui/Icon/Icon.stories.tsx
+++ src/shared/ui/Icon/Icon.stories.tsx
@@ -30,7 +30,7 @@ const meta: Meta<typeof Icon> = {
   argTypes: {
     name: {
       control: { type: 'select' },
-      options: ['shared-add', 'shared-check', 'shared-close'],
+      options: ['shared-add', 'shared-check', 'shared-close', 'idea'],
       description:
         'Часть после `icon-` в id символа внутри спрайта. Например: `shared-add` для `#icon-shared-add`',
     },
